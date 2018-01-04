9d49da0e9ddb7562a5e98cf7614ddc05f7a6c602
Brandon Wever <brandon@madebyatomicrobot.com>
2018-01-04 17:51:22 -0500

    Splash screen and icons

--- android/app/src/main/java/com/hackernews/MainActivity.java
+++ android/app/src/main/java/com/hackernews/MainActivity.java
@@ -1,8 +1,16 @@
 package com.hackernews;
 
+import android.graphics.Color;
+import android.widget.LinearLayout;
 import com.reactnativenavigation.controllers.SplashActivity;
-
 
 public class MainActivity extends SplashActivity {
 
+    @Override public LinearLayout createSplashLayout() {
+        LinearLayout view = new LinearLayout(this);
+
+        view.setBackgroundColor(Color.parseColor("#FB651E"));
+
+        return view;
+    }
 }
