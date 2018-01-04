863085c0cae421889612172fa3e4d4ed48b57d47
Brandon Wever <brandon@madebyatomicrobot.com>
2018-01-04 17:18:52 -0500

    Async tests

--- Actions/HackerNewsActions.test.js
+++ Actions/HackerNewsActions.test.js
@@ -1,8 +1,82 @@
 import * as actions from "./HackerNewsActions";
+import fetchMock from "fetch-mock";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
 
-describe("HackerNews synchronous Actions", () => {});
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
-describe("HackerNews synchronous Actions", () => {
+describe("HackerNews Async Actions", () => {
+  afterEach(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+  });
+
+  it("can request story ids", () => {
+    const category = "newstories";
+    const storyIds = [1];
+    fetchMock.getOnce(`end:${category}.json`, {
+      body: storyIds
+    });
+    fetchMock.getOnce(`end:/item/${storyIds[0]}.json`, {
+      body: { title: "Story 1" }
+    });
+
+    const expectedActions = [
+      {
+        type: actions.ActionTypes.storyIdsRequested,
+        category
+      },
+      {
+        type: actions.ActionTypes.storyIdsReceived,
+        ids: storyIds,
+        category
+      },
+      {
+        category,
+        ids: storyIds,
+        type: actions.ActionTypes.storiesRequested
+      }
+    ];
+
+    const store = mockStore({ newstories: {} });
+    return store.dispatch(actions.storyIdsRequested(1, category)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  it("can request stories", () => {
+    const storyIds = [1, 2];
+    fetchMock.getOnce(`end:/item/${storyIds[0]}.json`, {
+      body: { title: "Story 1" }
+    });
+    fetchMock.getOnce(`end:/item/${storyIds[1]}.json`, {
+      body: { title: "Story 2" }
+    });
+
+    const expectedActions = [
+      {
+        type: actions.ActionTypes.storiesRequested,
+        ids: storyIds,
+        category: "newstories"
+      },
+      {
+        category: "newstories",
+        stories: [{ title: "Story 1" }, { title: "Story 2" }],
+        type: actions.ActionTypes.storiesReceived
+      }
+    ];
+
+    const store = mockStore({ newstories: {} });
+    return store
+      .dispatch(actions.storiesRequested(storyIds, "newstories"))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+  });
+});
+
+describe("HackerNews Synchronous Actions", () => {
   it("should create a story ids received action", () => {
     const storyIds = [1, 2, 3];
     const category = "category";
